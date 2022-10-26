6a0f0afcab8537cc57925021fb5eb9bd193f19f2
Adrien Jaillet <adrien.jaillet@insa-lyon.fr>
2022-10-26 18:00:45 +0200

    Add a log

--- v1/Dna.cpp
+++ v1/Dna.cpp
@@ -11,6 +11,12 @@ Dna::Dna(int length, Threefry::Gen &&rng) : seq_(length) {
     for (int32_t i = 0; i < length; i++) {
         seq_[i] = '0' + rng.random(NB_BASE);
     }
+    for (auto &elem: seq_)
+    {
+        std::cout << elem;
+    }
+    std::cout << "\n";
+    std::cout << length << "\n";
 }
 
 int Dna::length() const {
