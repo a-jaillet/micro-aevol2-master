3c3586a23c651249470b0464bba896b60710d301
Adrien Jaillet <adrien.jaillet@insa-lyon.fr>
2022-11-22 11:48:36 +0000

    Add a V4

---
+++ v4/bin/compile_both.sh
@@ -0,0 +1,16 @@
+CURRENT_PATH="${MICRO_AEVOL_PROJECT_DIRECTORY}/v4/bin"
+
+cd $CURRENT_PATH
+
+mkdir -p ../build
+cd ../build 
+cmake ..
+make
+
+mkdir -p ../experiments
+
+cp micro_aevol_cpu ../experiments/micro_aevol_cpu
+
+cmake .. -DUSE_CUDA=on
+make
+cp micro_aevol_gpu ../experiments/micro_aevol_gpu
